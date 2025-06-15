e5da50842f001071c8f66f3e0ebdc79fdfd47d0f
Partofcosmos1-re <dip868610@gmail.com>
2025-06-15 22:12:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.post("/api/gemini", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => console.log(`Listening on ${port}`));
